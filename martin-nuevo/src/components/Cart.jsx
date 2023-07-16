8ddb279e6102c9e3e4eb093ab9bd77f1d9d3559e
Martin Slavsky <martinboca22@yahoo.com.ar>
2023-07-16 16:14:44 -0300

    cart

--- martin-nuevo/src/components/Cart.jsx
+++ martin-nuevo/src/components/Cart.jsx
@@ -3,15 +3,49 @@ import { CartContext } from "./CartContext";
 
 
 const Cart = () => {
-    const {cart, removeItem, clear, cartTotal, sumTotal} = useContext(CartContext);
+    const {cart, cartTotal, sumTotal} = useContext(CartContext);
+
+    if (cartTotal() === 0) {
+        return (
+            <div className="container my-4">
+                <div className="row">
+                    <div className="col text-center">
+                        <div className="alert alert-danger" role="alert">No se encontraron productos en el carrito</div>
+                    </div>
+                </div>
+            </div>  
+        )
+    }
     
     return (
-        <div>
-            <h3>Cart</h3>
-            <p>Total Producto: {cartTotal()}</p>
-            <ul>
-                {cart.map(items => <li>{items.titulo}</li>)}
-            </ul>
+        <div className="container">
+            <div className="row">
+                <div className="col text-center">
+                    <h1>Productos Seleccionados en su Compra</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <table className="table">
+                    {
+                        cart.map(item => (
+                            <tr>
+                                <td><img src={item.imagen} alt={"item.titulo"} width={70} /></td>
+                                <td>{item.titulo}</td>
+                                <td>${item.cantidad * item.precio}</td>
+                            </tr>
+                        ))
+                    }
+
+                    <tr>
+                        <td colSpan={3}>Total a Pagar</td>
+                        <td>${sumTotal()}</td>
+                        <td>&nbsp;</td>
+                    </tr>
+
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
--- martin-nuevo/src/components/CartWidget.jsx
+++ martin-nuevo/src/components/CartWidget.jsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
 import carrocompra from "./images/carro-compra.png";
+import { CartContext } from "./CartContext";
+import { useContext } from "react";
 
 const CartWidget = () => {
+    const {cartTotal} = useContext(CartContext);
+    
     return (
-        <button type="button" className="btn btn-light position-relative">
+        (cartTotal() > 0) ?
+        <Link className="btn btn-light position-relative" to={"/cart"}>
             <img src={carrocompra} alt={"carrocompra"} width={40} />
-            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">1</span>
-        </button>
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{cartTotal()}</span>
+        </Link> : "" 
     )
 }
 
